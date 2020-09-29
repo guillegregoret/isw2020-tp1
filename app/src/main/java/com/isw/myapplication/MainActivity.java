4014d39063aaf0826d31c0c7af4ece540a33c21c
fbusso <fbusso@frsf.utn.edu.ar>
2020-09-28 23:56:11 -0300

    Agregar botón en la pantalla principal.
    Primer modificación de la aplicación

--- app/src/main/java/com/isw/myapplication/MainActivity.java
+++ app/src/main/java/com/isw/myapplication/MainActivity.java
@@ -3,12 +3,25 @@ package com.isw.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button boton = (Button) findViewById(R.id.id_boton);
+        boton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(getApplicationContext(), "It's a feature", Toast.LENGTH_SHORT).show();
     }
 }
